f18b2c0bbb08cd46ca422d8c04b3349130472e6d
SavaSmiljkovic-LC <sava.smiljkovic@localsearch.ch>
2024-11-29 00:02:33 +0100

    first commit

---
+++ order/src/main/java/com/numarics/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.numarics.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
